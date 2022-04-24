a29c972159d55886fcb927322f295416184e637b
wonsss <bigdatajang@gmail.com>
2022-04-24 20:56:18 +0900

    refactor: addEventListenr 실행 단계 변경 및 removeEventListener 추가

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import Calculator from './Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Calculator />
-  </React.StrictMode>,
-);
+root.render(<Calculator />);
 
 reportWebVitals();
